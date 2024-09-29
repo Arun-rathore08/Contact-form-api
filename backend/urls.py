9220fe56e1f217f6508d0d35fc5c339b4a89243e
Arun-rathore08 <97669820+Arun-rathore08@users.noreply.github.com>
2024-09-29 23:09:31 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/contact/', include('contact.urls')),
+    path('api/contact/submit/', include('contact.urls')),
 ]
