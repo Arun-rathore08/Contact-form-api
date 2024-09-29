067185aea364a34f6e717e04fabb63b46ab94f48
Arun-rathore08 <97669820+Arun-rathore08@users.noreply.github.com>
2024-09-29 22:55:00 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lcc$e2wlwequfm==#ijw24f2bm9%v*b##)1492+k7*2z=gc!qx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
